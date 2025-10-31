1398ebab7c60ef8cab0d88ec7f9e2117835c8541
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2025-10-31 14:52:06 +0100

    first commit

--- index.test.js
+++ index.test.js
@@ -2,6 +2,15 @@ const multiply = require('./index');
 
 // TEST SUITE
 
-test("2 and 5 multiply equal 10", async()=>{
-    expect(multiply(2,5)).toBe(10); //exact equality
+describe("Multiply Test", () => {
+    it("2 and 5 multiply equal 10", async()=>{
+        expect(multiply(2,5)).toBe(10); //exact equality
+    });
+    test("2 and 5 multiply equal 10", async()=>{
+        expect(multiply(2,6)).not.toBe(10); //exact equality
+    });
 })
+
+// test("2 and 5 multiply equal 10", async()=>{
+//     expect(multiply(2,5)).toBe(10); //exact equality
+// })
